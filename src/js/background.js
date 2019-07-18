359964276f57d1a812bd7884736519f40736d907
Byunggyu Ahn <byunggyu.ahn@10gen.com>
2019-07-18 13:09:27 -0400

    Change the icon to the slack icon

--- src/js/background.js
+++ src/js/background.js
@@ -1,5 +1,6 @@
 import '../img/icon-128.png'
 import '../img/icon-34.png'
+import '../img/slack256.png'
 
 chrome.runtime.onInstalled.addListener(function() {
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
